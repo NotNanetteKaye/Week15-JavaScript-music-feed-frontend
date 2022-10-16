ca8e2d76a7e20f46c149ad74f7e6776d5e1d3138
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-10-15 20:10:37 -0500

    Import Axios (4): in Apps.js, import axios from 'axios'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
 
   useEffect(() => {
 
+
   }, []);
 
-  
+
   return (
     <div>
     </div>
