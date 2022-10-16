1e81945f0e151466e54a87b5cded5b9b1395a521
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-10-15 20:17:30 -0500

    Created Get Asynchronous Function

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import axios from 'axios';
 
 function App() {
 
-  useEffect(() => {
-
+  useEffect(async () => {
+    const response = await axios.get('http://127.0.0.1:8000/api/music/');
+    console.log(response);
 
   }, []);
 
