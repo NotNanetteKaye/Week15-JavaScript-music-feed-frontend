0bbd1247e3258175d0d344d8f68e8adde88ea579
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-10-15 20:26:37 -0500

    (5): Create useState variable

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 function App() {
 
+  const [songs, setSongs] = useState([]);
+
   useEffect(() => {
     getAllSongs();
   }, []);
@@ -10,6 +12,7 @@ function App() {
   async function getAllSongs(){
     const response = await axios.get('http://127.0.0.1:8000/api/music/');
     console.log(response.data);
+    setSongs(response.data)
   }
 
 
